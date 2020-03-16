31d39c05c0e54dd948503f0ad241d237230a551c
Maurice Falls <falls8019oct@gmail.com>
2020-03-16 00:08:11 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,6 +12,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
+    if #{age} > 13
     puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
